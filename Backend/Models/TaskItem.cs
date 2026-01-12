25893f49cc31a27fbc0387ef02de8c2939ecb450
Abdullah Muhammad Mahmoud <abdu.allah15@yahoo.com>
2026-01-12 07:25:23 +0200

    Add TaskItem Model With Data Annotations For Validation

---
+++ Backend/Models/TaskItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; } = String.Empty;
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
