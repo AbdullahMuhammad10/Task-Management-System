6541318e836b5de58cc8bd1577882efd50e99d9e
Abdullah Muhammad Mahmoud <abdu.allah15@yahoo.com>
2026-01-12 10:24:01 +0200

    Add Maximum Length Validation To Task Title

--- Backend/Dtos/AddTaskDto.cs
+++ Backend/Dtos/AddTaskDto.cs
@@ -6,7 +6,8 @@ namespace Backend.Dtos
     {
         [Required(AllowEmptyStrings = false)] // Title Is Required And Cannot Be An Empty String 
         [MinLength(3,ErrorMessage = "Title Must Be At Least 3 Characters")] // Add Minimum Length Validation As Extra Validation
-        public string Title { get; set; } = string.Empty;
+        [MaxLength(100,ErrorMessage = "Title Must Be At Less Than 100 Characters")] // Add Maximum Length Validation
+        public string? Title { get; set; }
         public string? Description { get; set; }
     }
 }
--- Backend/Dtos/UpdateTaskDto.cs
+++ Backend/Dtos/UpdateTaskDto.cs
@@ -6,6 +6,7 @@ namespace Backend.Dtos
     {
         [Required(AllowEmptyStrings = false)] // Title Is Required And Cannot Be An Empty String 
         [MinLength(3,ErrorMessage = "Title Must Be At Least 3 Characters")] // Add Minimum Length Validation As Extra Validation
+        [MaxLength(100,ErrorMessage = "Title Must Be At Less Than 100 Characters")] // Add Maximum Length Validation
         public string? Title { get; set; }
         public string? Description { get; set; }
         public bool? IsCompleted { get; set; }
