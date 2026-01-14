c4019b50be38339e84164808fa2fccb88eb81c58
Abdullah Muhammad Mahmoud <abdu.allah15@yahoo.com>
2026-01-14 08:41:17 +0200

    Implement Navbar Component

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskList } from './core/components/task-list/task-list';
 import { AddTask } from './core/components/add-task/add-task';
+import { Navbar } from './core/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
